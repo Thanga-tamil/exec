feaafa65883e7144c603f1055133bc8aac8458c7
thangatamil <thangatamil518@gmail.com>
2026-07-31 13:33:54 +0530

    CORS temp middleware added

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+    "github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+
 	"github.com/Thanga-tamil/exec/internal/config"
 	"github.com/Thanga-tamil/exec/internal/router"
 	logger "github.com/Thanga-tamil/exec/internal/utils"
 	consts "github.com/Thanga-tamil/exec/internal/utils"
-	"github.com/gin-gonic/gin"
 )
 
 
@@ -16,6 +18,13 @@ func main(){
 	addr := config.LoadConfig(consts.Path)
 
 	serve := gin.Default()
+
+    serve.Use(cors.New(cors.Config{
+        AllowOrigins:     []string{"https://cli-ent-react.thangatamil1177.workers.dev"},
+        AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+        AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+        AllowCredentials: true,
+    }))
 
 	cal := serve.Group("/api/cal")
 
