38ded272693df329e138aa2debe64aabc9b0f651
thangatamil <thangatamil518@gmail.com>
2026-07-31 09:17:19 +0530

    input validation updated

--- internal/handler/handle.go
+++ internal/handler/handle.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"errors"
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
@@ -16,44 +17,49 @@ import (
 func Add(c *gin.Context){
 	log.Println("calling lib Do function for addition")
 	
-	Type, err := validAndGetParsedVal(c, &c.Params, "type")
-
-	a, err := validAndGetParsedVal(c, &c.Params, "a")
-	b, err := validAndGetParsedVal(c, &c.Params, "b")
-
+	valType, err := validAndGetParsedVal(c, &c.Params, "type")
 	if err != nil {
 		c.JSON(http.StatusBadRequest, response.Error(err.Error(), 400)); return
 	}
 
+	a, err := validAndGetParsedVal(c, &c.Params, "a")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, response.Error(err.Error(), 400)); return
+	}
+
+	b, err := validAndGetParsedVal(c, &c.Params, "b")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, response.Error(err.Error(), 400)); return
+	}
+
+
 	valA, err := strconv.Atoi(a)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, response.Error(err.Error(), 400))
-		return
+		c.JSON(http.StatusBadRequest, response.Error(err.Error(), 400)); return
 	}
 	
 	valB, err := strconv.Atoi(b)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, response.Error(err.Error(), 400))
-		return 
+		c.JSON(http.StatusBadRequest, response.Error(err.Error(), 400)); return 
 	}
 
-	cal.Do(valA, valB, Type)
+	cal.Do(valA, valB, valType)
+}
+
+func validAndGetParsedVal(c *gin.Context, params *gin.Params, key string) (string, error) {
+
+	fmt.Println("key: ", key)
+	val := c.Query(key)
+
+	if val == ""  {
+		logger.Error("type input param must not be null or empty")
+		return "", errors.New("Input '" + key + "' must not be null or empty")
+	}
+
+	return val, nil
 }
 
 func Sub(c *gin.Context){
 	log.Println("calling lib Do function for subtraction")
 	// cal.Do()
-}
-
-func validAndGetParsedVal(c *gin.Context, params *gin.Params, key string) (string, error) {
-
-	val, exist := c.Params.Get(key)
-
-	if !exist {
-		logger.Error("type input param must not be null or empty")
-		c.JSON(http.StatusBadRequest, response.Error("Input 'type' must not be null or empty", 400))
-		return "", errors.New("null/empty")
-	}
-
-	return val, nil
 }
